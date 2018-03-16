c4476ef1680e425397699184559a60ef1ce3095c
Yuichi Ikeda <ikeda@framelunch.jp>
2018-03-16 10:53:43 +0900

    clearAllを追加

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 var Cp = require('obento-cp');
+/**
+ * keywordごとのlistenerの実行を行う
+ * @class
+ */
 var Notice = function() {
   this.cps = {};
 };
 Notice.prototype = {
+  /**
+   * @memberof Notice
+   * @param {string} key - listenerを管理する識別子
+   * @param {function} func - 指定した識別子に登録される関数
+   * @returns {function} 登録された関数
+   */
   listen: function(key, func) {
     var c = this.cps[key];
-    if (!c) {
-      c = this.cps[key] = new Cp();
-    }
+    if (!c) c = this.cps[key] = new Cp();
     c.add(func);
     return func;
   },
+
+  /**
+   * @memberof Notice
+   * @param {string} key - listenerを管理する識別子
+   * @param {function} args - arguments for called function.
+   */
+  publish: function(key, args) {
+    var c = this.cps[key];
+    if (!c) return;
+    c.update(args);
+  },
+
+  /**
+   * @memberof Notice
+   * @param {string} key - listenerを管理する識別子
+   * @param {function} func - 指定した識別子から削除される関数
+   */
   clear: function(key, func) {
     var c = this.cps[key];
     if (!c) return;
     c.remove(func);
   },
-  publish: function(key, obj) {
-    var c = this.cps[key];
-    if (!c) {
-      return;
-    }
-    c.update(obj);
-  },
-  release: function() {
-    var p;
-    for (p in this.cps) {
-      this.cps[p].release();
-    }
-    delete this.cps;
+
+  /**
+   * @memberof Notice
+   * @param {string} key - listenerを管理する識別子
+   */
+  clearAll: function(key) {
+    delete this.cps[key];
   }
 };
 
--- index.test.js
+++ index.test.js
@@ -1,11 +1,23 @@
 const Notice = require('./index');
 
-it('notice.add', () => {
+it('notice basic', () => {
   const n = new Notice();
-  const mock = jest.fn();
+  const mock1 = jest.fn();
+  const mock2 = jest.fn();
 
-  n.listen('n', mock);
+  n.listen('n', mock1);
+  n.listen('n', mock2);
+  n.publish('n', [10]);
+
+  n.clear('n', mock1);
+  n.publish('n', [11]);
+
+  n.clearAll('n');
   n.publish('n');
 
-  expect(mock.mock.calls.length).toBe(1);
+  expect(mock1.mock.calls.length).toBe(1);
+  expect(mock1.mock.calls[0][0]).toBe(10);
+  expect(mock2.mock.calls.length).toBe(2);
+  expect(mock2.mock.calls[0][0]).toBe(10);
+  expect(mock2.mock.calls[1][0]).toBe(11);
 });
